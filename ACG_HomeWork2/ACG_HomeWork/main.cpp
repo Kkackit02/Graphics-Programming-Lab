ae978b1c850dd6e6bd4985b8f2c9cac641f0a5b8
JungGeunnyeong <kkackit@konkuk.ac.kr>
2025-10-02 14:46:32 +0900

    EDIT | 세팅완료

--- ACG_HomeWork2/ACG_HomeWork/main.cpp
+++ ACG_HomeWork2/ACG_HomeWork/main.cpp
@@ -18,6 +18,8 @@
 */
 #include <glm/glm.hpp>
 
+
+#include "OBJ_Loader.h"
 #include "geometry.h"
 #include "Scene.h"
 
@@ -28,6 +30,13 @@ int g_windowWidth, g_windowHeight;
 
 GLuint programID ;
 GLuint programIDFur ;
+
+objl::Loader Loader;
+
+// Load .obj File
+bool loadout = Loader.LoadFile("cube.obj");
+
+
 GLuint LoadShadersSub(GLuint shader_type, const char* file_path)
 {
     GLuint shaderID = glCreateShader(shader_type);
@@ -159,6 +168,79 @@ void init()
 	glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
 	glEnable(GL_DEPTH_TEST);
 
+
+	// If so continue
+	if (loadout)
+	{
+		// Create/Open e1Out.txt
+		std::ofstream file("e1Out.txt");
+
+		// Go through each loaded mesh and out its contents
+		for (int i = 0; i < Loader.LoadedMeshes.size(); i++)
+		{
+			// Copy one of the loaded meshes to be our current mesh
+			objl::Mesh curMesh = Loader.LoadedMeshes[i];
+
+			// Print Mesh Name
+			file << "Mesh " << i << ": " << curMesh.MeshName << "\n";
+
+			// Print Vertices
+			file << "Vertices:\n";
+
+			// Go through each vertex and print its number,
+			//  position, normal, and texture coordinate
+			for (int j = 0; j < curMesh.Vertices.size(); j++)
+			{
+				file << "V" << j << ": " <<
+					"P(" << curMesh.Vertices[j].Position.X << ", " << curMesh.Vertices[j].Position.Y << ", " << curMesh.Vertices[j].Position.Z << ") " <<
+					"N(" << curMesh.Vertices[j].Normal.X << ", " << curMesh.Vertices[j].Normal.Y << ", " << curMesh.Vertices[j].Normal.Z << ") " <<
+					"TC(" << curMesh.Vertices[j].TextureCoordinate.X << ", " << curMesh.Vertices[j].TextureCoordinate.Y << ")\n";
+			}
+
+			// Print Indices
+			file << "Indices:\n";
+
+			// Go through every 3rd index and print the
+			//	triangle that these indices represent
+			for (int j = 0; j < curMesh.Indices.size(); j += 3)
+			{
+				file << "T" << j / 3 << ": " << curMesh.Indices[j] << ", " << curMesh.Indices[j + 1] << ", " << curMesh.Indices[j + 2] << "\n";
+			}
+
+			// Print Material
+			file << "Material: " << curMesh.MeshMaterial.name << "\n";
+			file << "Ambient Color: " << curMesh.MeshMaterial.Ka.X << ", " << curMesh.MeshMaterial.Ka.Y << ", " << curMesh.MeshMaterial.Ka.Z << "\n";
+			file << "Diffuse Color: " << curMesh.MeshMaterial.Kd.X << ", " << curMesh.MeshMaterial.Kd.Y << ", " << curMesh.MeshMaterial.Kd.Z << "\n";
+			file << "Specular Color: " << curMesh.MeshMaterial.Ks.X << ", " << curMesh.MeshMaterial.Ks.Y << ", " << curMesh.MeshMaterial.Ks.Z << "\n";
+			file << "Specular Exponent: " << curMesh.MeshMaterial.Ns << "\n";
+			file << "Optical Density: " << curMesh.MeshMaterial.Ni << "\n";
+			file << "Dissolve: " << curMesh.MeshMaterial.d << "\n";
+			file << "Illumination: " << curMesh.MeshMaterial.illum << "\n";
+			file << "Ambient Texture Map: " << curMesh.MeshMaterial.map_Ka << "\n";
+			file << "Diffuse Texture Map: " << curMesh.MeshMaterial.map_Kd << "\n";
+			file << "Specular Texture Map: " << curMesh.MeshMaterial.map_Ks << "\n";
+			file << "Alpha Texture Map: " << curMesh.MeshMaterial.map_d << "\n";
+			file << "Bump Map: " << curMesh.MeshMaterial.map_bump << "\n";
+
+			// Leave a space to separate from the next mesh
+			file << "\n";
+		}
+
+		// Close File
+		file.close();
+	}
+	// If not output an error
+	else
+	{
+		// Create/Open e1Out.txt
+		std::ofstream file("e1Out.txt");
+
+		// Output Error
+		file << "Failed to Load File. May have failed to find it or it was not an .obj file.\n";
+
+		// Close File
+		file.close();
+	}
 }
 
 void Animate(int value)
@@ -200,7 +282,6 @@ int main(int argc, char **argv)
 
 	//3. 
 	programID = LoadShaders("VertexShader.txt", "FragmentShader.txt");
-    programIDFur = LoadShaders("Fur_VertexShader.txt", "Fur_FragmentShader.txt","Fur_GeomShader.txt");
 	glUseProgram(programID);
     
     myProgram = programID;   
